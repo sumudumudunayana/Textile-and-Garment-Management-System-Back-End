6fdbe5da974da29686285cd438b909994b023e5e
sumudumudunayana <mudunayanamihigurathna@gmail.com>
2025-11-02 21:57:01 +0530

    Customer repository created

---
+++ src/main/java/org/example/repository/CustomerRepository.java
@@ -0,0 +1,7 @@
+package org.example.repository;
+
+import org.example.entity.CustomerEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface CustomerRepository extends JpaRepository<CustomerEntity, Integer> {
+}
