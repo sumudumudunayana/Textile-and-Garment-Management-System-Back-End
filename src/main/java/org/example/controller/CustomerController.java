dc9c6c3cefa0a6a616e324841f51ca07f366a97a
sumudumudunayana <mudunayanamihigurathna@gmail.com>
2025-11-02 21:56:34 +0530

    Customer controller created

---
+++ src/main/java/org/example/controller/CustomerController.java
@@ -0,0 +1,49 @@
+package org.example.controller;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.example.dto.Customer;
+import org.example.dto.UserAccount;
+import org.example.service.CustomerService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/customer")
+@RequiredArgsConstructor
+@CrossOrigin
+@Slf4j
+public class CustomerController {
+    @Autowired
+    final CustomerService service;
+
+    @GetMapping("/get-all")
+    public List<Customer> getCustomer(){
+        return service.getAll();
+    }
+
+    @PostMapping("/add-customer")
+    public void addCustomer(@RequestBody Customer customer){
+        log.info("updated-> {}",customer);
+        service.addCustomer(customer);
+    }
+
+    @GetMapping("/search-by-id/{id}")
+    public Customer getCustomerById(@PathVariable Integer id){
+        return service.searchCustomerById(id);
+    }
+
+    @DeleteMapping("/delete-by-id/{id}")
+    @ResponseStatus(HttpStatus.ACCEPTED)
+    public void deleteCustomerById(@PathVariable Integer id){
+        service.deleteCustomerById(id);
+    }
+    @PutMapping("/update-customer")
+    @ResponseStatus(HttpStatus.ACCEPTED)
+    public void updateCustomer(@RequestBody Customer customer){
+        service.updateCustomerById(customer);
+    }
+}
