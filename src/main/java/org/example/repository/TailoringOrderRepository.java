925d4aa2e49ffc9861b6f9aa457b3ce1644abf02
sumudumudunayana <mudunayanamihigurathna@gmail.com>
2025-12-01 19:36:09 +0530

    tailoring order repository created

---
+++ src/main/java/org/example/repository/TailoringOrderRepository.java
@@ -0,0 +1,7 @@
+package org.example.repository;
+
+import org.example.entity.TailoringOrderEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface TailoringOrderRepository extends JpaRepository<TailoringOrderEntity, Integer> {
+}
