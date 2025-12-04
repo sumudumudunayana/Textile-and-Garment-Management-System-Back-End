777f89038c3be9d1aa72f592b550f5677e150e86
sumudumudunayana <mudunayanamihigurathna@gmail.com>
2025-12-04 23:50:45 +0530

    login request added

---
+++ src/main/java/org/example/dto/LoginRequest.java
@@ -0,0 +1,43 @@
+package org.example.dto;
+
+import java.util.Objects;
+
+public class LoginRequest {
+    private final String username;
+    private final String password;
+
+    public LoginRequest(String username, String password) {
+        this.username = username;
+        this.password = password;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof LoginRequest)) return false;
+        LoginRequest that = (LoginRequest) o;
+        return Objects.equals(username, that.username) &&
+                Objects.equals(password, that.password);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(username, password);
+    }
+
+    @Override
+    public String toString() {
+        return "LoginRequest{" +
+                "username='" + username + '\'' +
+                ", password='" + password + '\'' +
+                '}';
+    }
+}
